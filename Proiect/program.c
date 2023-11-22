70fb5e8d30a3cbd52673e8668a522f1822e88920
Adrian Solomon <adrian.solomon@student.upt.ro>
2023-11-22 20:08:50 +0200

    Cleaned up even more

--- Proiect/program.c
+++ Proiect/program.c
@@ -11,8 +11,14 @@
 #include <dirent.h>
 
 #define BUFF_SIZE 4096
+#define OUT_FILE "statistica.txt"
+typedef struct rights {
+    char user_rights[4];
+    char group_rights[4];
+    char other_rights[4]; 
+} rights;
 
-void rigths(struct stat fileStats, char* user, char* group, char* other)
+void getRigths(struct stat fileStats, char* user, char* group, char* other)
 {
     strcat(user, (fileStats.st_mode & S_IRUSR) ? "R" : "-");
     strcat(user, (fileStats.st_mode & S_IWUSR) ? "W" : "-");
@@ -27,18 +33,78 @@ void rigths(struct stat fileStats, char* user, char* group, char* other)
     strcat(other, (fileStats.st_mode & S_IXOTH) ? "X" : "-");
 }
 
+void processBMP(int fin, struct dirent* entry, char *date, struct stat fileStats, char *foutContent, rights file_rights) {
+    unsigned int h = 0, w = 0;
+    if(lseek(fin, 18, SEEK_SET) == -1) {
+        perror("Error setting cursor!\n");
+        close(fin);
+        exit(-1);
+    }
+    read(fin, &w, 4);
+    read(fin, &h, 4);  
+    fflush(NULL);
+    sprintf(foutContent, "nume fisier: %s\n"
+                         "inaltime: %d\n"
+                         "lungime: %d\n"
+                         "dimensiune: %ld By\n"
+                         "identificatorul utilizatorului: %d\n"
+                         "timpul ultimei modificari: %s\n"
+                         "contorul de legaturi: %ld\n"
+                         "drepturi de acces user: %s\n"
+                         "drepturi de acces grup: %s\n"
+                         "drepturi de acces altii: %s\n\n", 
+            entry->d_name, h, w, 
+            fileStats.st_size, fileStats.st_uid, date, fileStats.st_nlink, 
+            file_rights.user_rights, file_rights.group_rights, file_rights.other_rights);
+}
+
+void processFile(struct dirent* entry, char *date, struct stat fileStats, char *foutContent, rights file_rights) {
+    sprintf(foutContent, "nume fisier: %s\n"
+                            "dimensiune: %ld By\n"
+                            "identificatorul utilizatorului: %d\n"
+                            "timpul ultimei modificari: %s\n"
+                            "contorul de legaturi: %ld\n"
+                            "drepturi de acces user: %s\n"
+                            "drepturi de acces grup: %s\n"
+                            "drepturi de acces altii: %s\n\n", 
+            entry->d_name, fileStats.st_size, fileStats.st_uid, date, 
+            fileStats.st_nlink, 
+            file_rights.user_rights, file_rights.group_rights, file_rights.other_rights);
+}
+
+void processSymbolicLink(struct dirent* entry, char *path_to_entry, struct stat fileStats, char *foutContent, rights file_rights) {
+    struct stat target_fileStats;
+    if(stat(path_to_entry, &target_fileStats) == -1) {
+        perror("Error getting target file information for symbolic link!\n");
+        exit(-1);
+    }
+    sprintf(foutContent, "nume legatura: %s\n"
+                            "dimensiune legatura: %ld\n"
+                            "dimensiune fisier: %ld\n"
+                            "drepturi de acces user: %s\n"
+                            "drepturi de acces grup: %s\n"
+                            "drepturi de acces altii: %s\n\n",
+            entry->d_name, fileStats.st_size, target_fileStats.st_size, file_rights.user_rights, file_rights.group_rights, file_rights.other_rights);
+}
+
+void processDirectory(struct dirent* entry, struct stat fileStats, char *foutContent, rights file_rights) {
+    snprintf(foutContent, strlen(foutContent), "nume director: %s\n"
+                                                "identificatorul utilizatorului: %d\n"
+                                                "drepturi de acces user: %s\n"
+                                                "drepturi de acces grup: %s\n"
+                                                "drepturi de acces altii: %s\n\n",
+            entry->d_name, fileStats.st_uid, file_rights.user_rights, file_rights.group_rights, file_rights.other_rights);
+}
+
 void generateStats(DIR* directory, int fout, char *dirpath) {
+    //data declaration
     char foutContent[BUFF_SIZE+1];
     char path_to_entry[BUFF_SIZE+1];
-    char user_rights[4] = "", group_rights[4] = "", other_rights[4] = ""; 
     char date[20];
-    long int fileSize = 0;
-    unsigned int h = 0, w = 0;
     int fin = 0;
-
+    rights file_rights;
     struct dirent* entry = NULL;
     struct stat fileStats;
-    struct stat target_fileStats;
 
     // cycling through the directory
     while((entry = readdir(directory)) != NULL) {
@@ -50,106 +116,58 @@ void generateStats(DIR* directory, int fout, char *dirpath) {
         snprintf(path_to_entry, sizeof(path_to_entry), "%s/%s", dirpath, entry->d_name);
 
         // trying to get file stats
-        if(lstat(path_to_entry, &fileStats) == 0) {
-            fileSize = fileStats.st_size;
-
-        } else {
+        if(lstat(path_to_entry, &fileStats) != 0) {
             printf("Error: Can not get file stats!\n");
+            exit(-1);
+
+        }
+
+        //get rights and last modify time for file
+        strftime(date, sizeof(date), "%d.%m.%Y", localtime(&fileStats.st_mtime));
+        getRigths(fileStats, file_rights.user_rights, file_rights.group_rights, file_rights.other_rights);
+
+        //oppening entry file
+        if ((fin = open(path_to_entry, O_RDONLY)) == -1) {
+            perror("Error opening input file!\n");
             exit(-1);
         }
 
         //check for file
          if(S_ISREG(fileStats.st_mode)) {
-            //oppening the file
-            if ((fin = open(path_to_entry, O_RDONLY)) == -1) {
-                perror("Error opening input file!\n");
-                exit(-1);
-            }
-
             // check for .bmp file
             if(strstr(entry->d_name, ".bmp") != NULL) {
-                if(lseek(fin, 18, SEEK_SET) == -1) {
-                    perror("Error setting cursor!\n");
-                    close(fin);
-                    exit(-1);
-                }
+                processBMP(fin, entry, date, fileStats, foutContent, file_rights);
 
-                read(fin, &w, 4);
-                read(fin, &h, 4);  
-                strftime(date, sizeof(date), "%d.%m.%Y", localtime(&fileStats.st_mtime));
-                rigths(fileStats, user_rights, group_rights, other_rights);
-                fflush(NULL);
-
-                sprintf(foutContent, "nume fisier: %s\n"
-                                     "inaltime: %d\n"
-                                     "lungime: %d\n"
-                                     "dimensiune: %ld By\n"
-                                     "identificatorul utilizatorului: %d\n"
-                                     "timpul ultimei modificari: %s\n"
-                                     "contorul de legaturi: %ld\n"
-                                     "drepturi de acces user: %s\n"
-                                     "drepturi de acces grup: %s\n"
-                                     "drepturi de acces altii: %s\n\n", 
-                        entry->d_name, h, w, fileSize, fileStats.st_uid, date, fileStats.st_nlink, user_rights, group_rights, other_rights);
-                
-                if(close(fin) != 0) {
-                    perror("Error! Could not close file %s !\n");
-                    exit(-1); 
-                }
             // check for ordinary file
             } else {
-                strftime(date, sizeof(date), "%d.%m.%Y", localtime(&fileStats.st_mtime));
-                rigths(fileStats, user_rights, group_rights, other_rights);
-
-                sprintf(foutContent, "nume fisier: %s\n"
-                                     "dimensiune: %ld By\n"
-                                     "identificatorul utilizatorului: %d\n"
-                                     "timpul ultimei modificari: %s\n"
-                                     "contorul de legaturi: %ld\n"
-                                     "drepturi de acces user: %s\n"
-                                     "drepturi de acces grup: %s\n"
-                                     "drepturi de acces altii: %s\n\n", 
-                        entry->d_name, fileSize, fileStats.st_uid, date, fileStats.st_nlink, user_rights, group_rights, other_rights);
+                processFile(entry, date, fileStats, foutContent, file_rights);
             }
 
         // check for symbolic link
          } else if(S_ISLNK(fileStats.st_mode)) {
-            if(stat(path_to_entry, &target_fileStats) == -1)
-            {
-                perror("Error getting target file information for symbolic link!\n");
-                exit(-1);
-            }
-
-            rigths(fileStats, user_rights, group_rights, other_rights);
-
-            sprintf(foutContent, "nume legatura: %s\n"
-                                 "dimensiune legatura: %ld\n"
-                                 "dimensiune fisier: %ld\n"
-                                 "drepturi de acces user: %s\n"
-                                 "drepturi de acces grup: %s\n"
-                                 "drepturi de acces altii: %s\n\n",
-                    entry->d_name, fileStats.st_size, target_fileStats.st_size, user_rights, group_rights, other_rights);
+            processSymbolicLink(entry, path_to_entry, fileStats, foutContent, file_rights);
 
         // check for directory
          } else if(S_ISDIR(fileStats.st_mode)) {
-            rigths(fileStats, user_rights, group_rights, other_rights);
-            
-            snprintf(foutContent, sizeof(foutContent), "nume director: %s\n"
-                                                       "identificatorul utilizatorului: %d\n"
-                                                       "drepturi de acces user: %s\n"
-                                                       "drepturi de acces grup: %s\n"
-                                                       "drepturi de acces altii: %s\n\n",
-                    entry->d_name, fileStats.st_uid, user_rights, group_rights, other_rights);
+            processDirectory(entry, fileStats, foutContent, file_rights);
          }
 
-        strcpy(date, "");
-        strcpy(user_rights, "");
-        strcpy(group_rights, "");
-        strcpy(other_rights, "");
+        // closing entry file
+        if(close(fin) != 0) {
+            perror("Error! Could not close file!\n");
+            exit(-1); 
+        }
 
+        // reseting rights and last modify time for file
+        strcpy(date, "");
+        strcpy(file_rights.user_rights, "");
+        strcpy(file_rights.group_rights, "");
+        strcpy(file_rights.other_rights, "");
+
+        // writing to statistica.txt
         if(strcmp(foutContent, "") != 0) {
             if (write(fout, foutContent, strlen(foutContent)) == -1) {
-                perror("Error writing to output file");
+                perror("Error writing to output file!");
                 close(fout);
                 exit(-1);
             }
@@ -175,7 +193,7 @@ int main(int argc, char **argv) {
 
     // creating & opening output file
 
-    int fout = open("statistica.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    int fout = open(OUT_FILE, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (fout == -1) {
         perror("Error opening output file!\n");
         exit(-1);
